0d52ad2b2ed34d01456e13c3c45d7a13209e9fea
Jorge <jorgediazok@gmail.com>
2021-05-14 02:35:13 -0300

    added models

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,9 +2,48 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
 
 router.get('/', (req, res) => res.send('Hello world'));
-router.get('/register', (req, res) => res.send('Hello from register'));
+router.post('/register', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ msg: 'Please fill all the fields' });
+    }
+
+    const user = await userSchema.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ msg: 'User already exists' });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const passwordHashed = await bcrypt.hash(password, salt);
+
+    const newUser = await new userSchema({
+      name,
+      email,
+      password: passwordHashed,
+    });
+
+    newUser.save();
+
+    res.json(newUser);
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ msg: 'Password must have at least 6 characters.' });
+    }
+
+    res.json({ msg: 'Success' });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 router.get('/login', (req, res) => res.send('Hello from Login'));
 
 module.exports = router;
